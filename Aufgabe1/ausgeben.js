20f2a64bfd98e7b6250ce1e2b6f12548db132dfd
mbudde4 <32923728+mbudde4@users.noreply.github.com>
2018-04-17 12:33:09 +0200

    ansgeben.js wurde weiter bearbeitet

--- Aufgabe1/ausgeben.js
+++ Aufgabe1/ausgeben.js
@@ -5,11 +5,17 @@ var content;
 
 fs.readFile("staedte.json",	function(err,	data)	{
 
-    var obj = JSON.parse(data);
-    JSON.stringify(obj(cities));
+
     content = data.toString();
+    content = content.replace(/[{}]/g, '');
+    content = content.replace(/["]/g, '');
+    //content = content.replace(/[,]/g, '');
+    content = content.replace(/[[]/g, '');
+    //content = content.replace(/[]/g, '');
+    content = content.replace("cities:", '');
 
 
+   // var myobj = JSON.parse(JSON.stringify(content));
         console.log(content);
 
 
