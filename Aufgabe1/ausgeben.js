6f2efbf49b01ed7180ea75af35ecd963012bbc98
mbudde4 <32923728+mbudde4@users.noreply.github.com>
2018-04-17 14:14:04 +0200

    Lösung Aufgabe1 Teil1

--- Aufgabe1/ausgeben.js
+++ Aufgabe1/ausgeben.js
@@ -9,14 +9,24 @@ fs.readFile("staedte.json",	function(err,	data)	{
     content = data.toString();
     content = content.replace(/[{}]/g, '');
     content = content.replace(/["]/g, '');
-    //content = content.replace(/[,]/g, '');
-    content = content.replace(/[[]/g, '');
-    //content = content.replace(/[]/g, '');
+    content = content.replace(/\[/g,"").replace(/\]/g,"");
     content = content.replace("cities:", '');
+
+    var array = content.split(' ,');
+    //console.log(array[0]);
 
 
    // var myobj = JSON.parse(JSON.stringify(content));
-        console.log(content);
+    //console.log(content);
+
+    var i = 0;
+
+    while (i < array.length)
+    {
+        console.log(array[i]);
+        console.log("      ---------------");
+        i++;
+    }
 
 
 });
