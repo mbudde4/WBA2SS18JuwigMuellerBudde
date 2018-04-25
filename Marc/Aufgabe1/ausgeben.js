9d542e4c6eae106a47965bbec075a478de5cb659
mbudde4 <32923728+mbudde4@users.noreply.github.com>
2018-04-25 12:03:27 +0200

    Versuche Aufgab1 Teil3 zu lösen

--- Marc/Aufgabe1/ausgeben.js
+++ Marc/Aufgabe1/ausgeben.js
@@ -17,24 +17,36 @@ fs.readFile("staedte.json",	function(err,	data)	{
     //cities und eckige klammern werden rausgeschmissen damit man später parsen kann
     str = str.replace(/\{"cities":\[/g,'');
     str = str.replace(/\]}/g,'');
+    /*str = str.replace(/"name"/g, 'name');
+    str = str.replace(/"country"/g, 'country');
+    str = str.replace(/"population"/g, 'population');*/
     str = str.replace(/},/g,'} , ');
-
+    console.log(str);
     //Splittet den String in ein Array
     var array = str.split(' , ');
 
+    function compareNumbers(a, b) {
+        return a - b;
+    }
+    array.sort(function (a, b) {
+        return a.population - b.population;
+    });
+    console.log(array);
     //console.log(str);
 
     var i = 0;
 
-    while (i < array.length-1)
+    while (i < array.length)
     {
         var obj = JSON.parse(array[i]);
+
         console.log("name: " + chalk.red(obj.name));
         console.log("country: " + chalk.green(obj.country));
         console.log("population: " + chalk.blue(obj.population));
         console.log("------------------");
         i++;
     }
+
 
 
 });
