ecafee92d1d88d580d2d771cf3811f4d52a9e895
mbudde4 <32923728+mbudde4@users.noreply.github.com>
2018-04-27 10:10:30 +0200

    Lösung Aufgabe1 komplett in Ordner Marc

--- Marc/Aufgabe1/ausgeben.js
+++ Marc/Aufgabe1/ausgeben.js
@@ -18,33 +18,46 @@ fs.readFile("staedte.json",	function(err,	data)	{
     str = str.replace(/\{"cities":\[/g,'');
     str = str.replace(/\]}/g,'');
 
-    str = str.replace(/},/g,'} , ');
-    console.log(str);
-    //Splittet den String in ein Array
-    var staedte = str.split(' , ');
-
-
-
-
-
-    console.log(staedte);
     //console.log(str);
 
-    var i = 0;
+    //Macht aus dem String ein json array und kein String array so wie split
+    var staedte = JSON.parse("[" + str + "]");
 
+    //sortiert nach population
+    staedte.sort(function (a, b) {
+        return a.population - b.population;
+    });
+
+    var string = JSON.stringify(staedte);
+    string = string.replace(/\[/g,'{"cities":\[');
+    string = string.replace(/\]/g,'\]}');
+    console.log(string);
+    fs.writeFile("staedte_sortiert.json", string, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });
+
+
+
+    //console.log(staedte);
+
+    //Ausgabe
+    var i = 0;
 
     while (i < staedte.length)
     {
-        var obj = JSON.parse(staedte[i]);
+       //var object = JSON.parse(staedte[i]);
 
-        console.log("name: " + chalk.red(obj.name));
-        console.log("country: " + chalk.green(obj.country));
-        console.log("population: " + chalk.blue(obj.population));
+
+        console.log("name: " + chalk.red(staedte[i].name));
+        console.log("country: " + chalk.green(staedte[i].country));
+        console.log("population: " + chalk.blue(staedte[i].population));
         console.log("------------------");
         i++;
     }
-
-
 
 });
 
