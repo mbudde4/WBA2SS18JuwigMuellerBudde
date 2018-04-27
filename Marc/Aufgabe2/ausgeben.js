c8c4370baedc8f8ae6c97bc30b01f04519092642
mbudde4 <32923728+mbudde4@users.noreply.github.com>
2018-04-27 11:34:53 +0200

    Nur Formatierung

--- Marc/Aufgabe2/ausgeben.js
+++ Marc/Aufgabe2/ausgeben.js
@@ -9,43 +9,33 @@ fs.readFile("staedte.json",	function(err,	data)	{
     var obj1 = JSON.parse(content1);
     content1 = JSON.stringify(obj1);
 
+    //String wird angepasst
     content1 = content1.replace(/\{"cities":/g,'');
     content1 = content1.replace(/\]}/g,'\]');
 
-    //console.log(content1);
+    //json array wird erstellt
     var staedte1 = JSON.parse(content1);
-   // console.log(staedte1);
 
-
+    //callback
     fs.readFile("mehr_staedte.json",	function(err,	data)	{
-
 
         var content2 = data.toString();
 
         var obj2 = JSON.parse(content2);
         content2 = JSON.stringify(obj2);
-
+        //String wird angepasst
         content2 = content2.replace(/\{"cities":/g,'');
         content2 = content2.replace(/\]}/g,'\]');
-
-       // console.log(content2);
-
+        //zweites json array wird erstellt
         var staedte2 = JSON.parse(content2);
-        //console.log(staedte2);
 
-
-
+        //staedte1 und staedte2 werden zusammengeführt
         var alleStaedte = staedte1.concat(staedte2);
-
-        //console.log(alleStaedte);
 
         //Ausgabe
         var i = 0;
 
-        while (i < alleStaedte.length)
-        {
-            //var object = JSON.parse(staedte[i]);
-
+        while (i < alleStaedte.length) {
 
             console.log("name: " + chalk.red(alleStaedte[i].name));
             console.log("country: " + chalk.green(alleStaedte[i].country));
@@ -53,8 +43,7 @@ fs.readFile("staedte.json",	function(err,	data)	{
             console.log("------------------");
             i++;
         }
-
-
+        
     });
 
 });
