393832d665f5e2bec1217e8b2cc82a6687f51f79
Marjuw <33716855+Marjuw@users.noreply.github.com>
2018-05-02 17:57:03 +0200

    test 2

--- Marvin/Aufgabe2/Promises.js
+++ Marvin/Aufgabe2/Promises.js
@@ -1,16 +1,32 @@
 var	fs=require('fs');
 const chalk=require('chalk');
 
+var obj1;
+var obj2;
+var content1;
+var content2;
+var staedte1;
+var staedte2;
+var err1=0;
+var err2=0;
+
+
 
 
 
 var p = new Promise(function(resolve, reject) {
 
+
     fs.readFile("staedte.json",	function(err,	data)	{
 
-        var content1 = data.toString();
+        if (err==true)
+        {
+        err1=err;
 
-        var obj1 = JSON.parse(content1);
+
+        content1 = data.toString();
+
+        obj1 = JSON.parse(content1);
         content1 = JSON.stringify(obj1);
 
         //String wird angepasst
@@ -18,35 +34,65 @@ var p = new Promise(function(resolve, reject) {
         content1 = content1.replace(/\]}/g,'\]');
 
         //json array wird erstellt
-        var alleStaedte = JSON.parse(content1);
+        staedte1 = JSON.parse(content1);
 
-        //Ausgabe
-        var i = 0;
 
-        while (i < alleStaedte.length) {
+    });
 
-            console.log("name: " + chalk.red(alleStaedte[i].name));
-            console.log("country: " + chalk.green(alleStaedte[i].country));
-            console.log("population: " + chalk.blue(alleStaedte[i].population));
-            console.log("------------------");
-            i++;
-        }
+    fs.readFile("mehr_staedte.json",	function(err,	data)	{
+
+        err2=err;
+
+        content2 = data.toString();
+
+        obj2 = JSON.parse(content2);
+        content2 = JSON.stringify(obj2);
+
+        //String wird angepasst
+        content2 = content2.replace(/\{"cities":/g,'');
+        content2 = content2.replace(/\]}/g,'\]');
+
+        //json array wird erstellt
+        staedte2 = JSON.parse(content1);
+
+
     });
 
 
-        if() {
-            reject('Failure!');
+    var test=0;
 
+        if(err!=0 || err2!=0) {
+
+            resolve('Success!');
         }
         else {
-                resolve('Success!');
+            reject('Failure!');
         }
+
+
 
 
 });
 
 p.then(function() {
 
+    console.log("Kein Fehler");
+
+    var alleStaedte = staedte1.concat(staedte2);
+
+
+    //Ausgabe
+    var i = 0;
+
+
+    while (i < alleStaedte.length) {
+
+        console.log("name: " + chalk.red(alleStaedte[i].name));
+        console.log("country: " + chalk.green(alleStaedte[i].country));
+        console.log("population: " + chalk.blue(alleStaedte[i].population));
+        console.log("------------------");
+        i++;
+    }
 
 }).catch(function() {
 
