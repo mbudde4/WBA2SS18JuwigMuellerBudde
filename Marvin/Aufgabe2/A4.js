e1129ba560591825529c3de4380b58a85835753c
Marjuw <33716855+Marjuw@users.noreply.github.com>
2018-05-07 12:38:48 +0200

    weiter...

--- Marvin/Aufgabe2/A4.js
+++ Marvin/Aufgabe2/A4.js
@@ -1,5 +1,6 @@
 
 //nr1
+/*
 for ( var x = 0; x < 10; x++) {
 
     console.log(x);
@@ -7,57 +8,21 @@ for ( var x = 0; x < 10; x++) {
     setTimeout(function () { console.log("The number is" + x);}, 100);
 
 }
+*/
 
 //nr2
 function getTweets( id {
-    return fetch ( ”https://api.twitter.com/user/” + id)
+    return fetch ( "https://api.twitter.com/user/" + id)
         .then(function (response) {
             return JSON.parse(response);
         }).then(function (response) {
-            return response.data;}).
-    then
-    (
-        function
-            (
-                tweets
-            ) {
-            return
-            tweets
-                .
-                filter
-                (
-                    function
-                        (
-                            tweet
-                        ) {
-                        return
-                        tweet
-                            .
-                            stars
-                        >
-                        50
-                    })
+            return response.data;}).then(function (tweets) {
+            return tweets.filter(function (tweet) {
+                        return tweet.stars > 50
+            })
         }).
-    then
-    (
-        function
-            (
-                tweets
-            ) {
-            return
-            tweets
-                .
-                filter
-                (
-                    function
-                        (
-                            tweet
-                        ) {
-                        return
-                        tweet
-                            .
-                            rts
-                        > 50
-                    })
-        })
+    then(function (tweets) {
+            return tweets.filter(function (tweet) {
+                        return tweet.rts > 50})
+    })
 }
