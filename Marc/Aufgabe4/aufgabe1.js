3988f6bb5653f20e7fecd4cb798a3602e03b3300
Marjuw <33716855+Marjuw@users.noreply.github.com>
2018-05-07 14:46:06 +0200

    update

--- Marc/Aufgabe4/aufgabe1.js
+++ Marc/Aufgabe4/aufgabe1.js
@@ -1,7 +1,9 @@
 
 
 for(var x = 0; x < 10; x++) {
+
     console.log(x);
+
     setTimeout(function() {
         console.log('The number is'  + x);
     }, 5000);
---
+++ Marvin/Aufgabe4/A4.js
@@ -0,0 +1,31 @@
+
+//4.1:
+/*
+for(var x = 0; x < 10; x++) {
+
+    console.log(x);
+
+    setTimeout(function() {
+        console.log('The number is'  + x);
+    }, 1000);
+
+}
+*/
+
+//4.2:
+function getTweets( id ) {
+    return fetch("https://api.twitter.com/user/" + id )
+        .then(function(response) {
+        return JSON.parse(response);
+    }).then( function(response) {
+        return response.data;
+    }).then(function(tweets) {
+        return tweets.filter(function(tweet) {
+            return tweet.stars > 50    })
+    }).then(function(tweets) {
+        return tweets.filter(function(tweet) {
+            return tweet.rts > 50
+        })
+    })
+}
+
