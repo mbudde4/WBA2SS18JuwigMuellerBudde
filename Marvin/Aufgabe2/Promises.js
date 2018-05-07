e23499b18f55282155e896b77341f34a9bc838d9
Marjuw <33716855+Marjuw@users.noreply.github.com>
2018-05-07 12:30:39 +0200

    Nr2+4 anfang

--- Marvin/Aufgabe2/Promises.js
+++ Marvin/Aufgabe2/Promises.js
@@ -14,110 +14,79 @@ var allestaedte;
 
 
 
-
-
-var p = new Promise(function(resolve, reject) {
-
-
+var p1 = new Promise(function(resolve, reject) {
 
 
     fs.readFile("staedte.json",	function(err,	data)	{
 
 
-
         content1 = data.toString();
 
         obj1 = JSON.parse(content1);
-        content1 = JSON.stringify(obj1);
 
-        //String wird angepasst
-        content1 = content1.replace(/\{"cities":/g,'');
-        content1 = content1.replace(/\]}/g,'\]');
+        obj1 = obj1.cities;
 
         //json array wird erstellt
-        staedte1 = JSON.parse(content1);
+        staedte1 = obj1;
 
-      /*  //Ausgabe1
-        var i = 0;
-
-
-        while (i < staedte1.length) {
-
-            console.log("name: " + chalk.red(staedte1[i].name));
-            console.log("country: " + chalk.green(staedte1[i].country));
-            console.log("population: " + chalk.blue(staedte1[i].population));
-            console.log("------------------");
-            i++;
-        }
-
-        */
 
         if (err==true) {
             err1 = err;
         }
 
+        if(err1!=0 || err2!=0) {
+
+            reject('Failure!');
+        }
+        else {
+
+            resolve('Success!');
+        }
 
     });
 
+});
+
+var p2 = new Promise(function(resolve, reject) {
+
+
     fs.readFile("mehr_staedte.json",	function(err,	data)	{
-
-
 
 
         content2 = data.toString();
 
         obj2 = JSON.parse(content2);
-        content2 = JSON.stringify(obj2);
 
-        //String wird angepasst
-        content2 = content2.replace(/\{"cities":/g,'');
-        content2 = content2.replace(/\]}/g,'\]');
+        obj2 = obj2.cities;
 
         //json array wird erstellt
-        staedte2 = JSON.parse(content2);
-
-        //Ausgabe2
-      /*  var i = 0;
-
-
-        while (i < staedte2.length) {
-
-          console.log("name: " + chalk.red(staedte2[i].name));
-          console.log("country: " + chalk.green(staedte2[i].country));
-          console.log("population: " + chalk.blue(staedte2[i].population));
-          console.log("------------------");
-          i++;
-        }
-
-      */
-
+        staedte2 = obj2;
 
 
         if (err==true) {
             err2 = err;
         }
 
+        if(err1!=0 || err2!=0) {
+
+            reject('Failure!');
+        }
+        else {
+
+            resolve('Success!');
+        }
+
 
     });
-
-
-    if(err1!=0 || err2!=0) {
-
-        reject('Failure!');
-    }
-    else {
-
-        resolve('Success!');
-    }
-
-
 });
 
-p.then(function() {
+
+
+Promise.all([p1,p2]).then(function() {
 
     console.log("Kein Fehler");
 
-    /* alleStaedte = staedte1.concat(staedte2);
+    alleStaedte = staedte1.concat(staedte2);
 
 
 
@@ -133,7 +102,7 @@ p.then(function() {
           console.log("------------------");
           i++;
       }
-      */
+
 
 }).catch(function() {
 
